4d9b50337f4e970b1e2126a70026e537d9ffceb5
shelton-je <jeremyshelton18@gmail.com>
2023-06-14 16:28:13 -0400

    Intial push

--- src/main/java/cs3500/pa05/Driver.java
+++ src/main/java/cs3500/pa05/Driver.java
@@ -1,11 +1,44 @@
 package cs3500.pa05;
 
+
+import cs3500.pa05.controller.JournalController;
+import cs3500.pa05.view.JournalView;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Represents a Java journal
+ */
 public class Driver extends Application {
+  /**
+   * Starts the GUI for a game of Whack-A-Mole.
+   *
+   * @param stage the JavaFX stage to add elements to
+   */
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage stage) {
+    JournalView JournalView  = new JournalView();
+    JournalController JournalController = new JournalController(JournalView);
 
+
+
+    try {
+      // load and place the view's scene onto the stage
+      Scene scene = JournalController.loadScene();
+      stage.setScene(scene);
+      // render the stage
+      stage.show();
+    } catch (IllegalStateException exc) {
+      System.err.println("Unable to load GUI.");
+    }
+  }
+  /**
+   * Entry point for java Journal
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch();
   }
 }
--- src/main/java/cs3500/pa05/controller/JournalController.java
+++ src/main/java/cs3500/pa05/controller/JournalController.java
@@ -1,4 +1,15 @@
 package cs3500.pa05.controller;
 
+import cs3500.pa05.view.JournalView;
+import javafx.scene.Scene;
+
 public class JournalController {
+
+
+  public JournalController(JournalView journalView) {
+  }
+
+  public Scene loadScene() {
+    return null;
+  }
 }
